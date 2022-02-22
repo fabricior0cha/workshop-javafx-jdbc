624c915c421e323c7d6745efc980eee3d8449757
unknown <fabriciorochapalmeira@gmail.com>
2022-02-22 14:02:25 -0300

    Department service

---
+++ src/model/services/DeparmentService.java
@@ -0,0 +1,18 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DeparmentService {
+
+	public List<Department> findAll(){
+		List<Department> list = new ArrayList<Department>();
+		list.add(new Department(1, "Books"));
+		list.add(new Department(2, "Eletronics"));
+		list.add(new Department(3, "Computers"));
+		return list;
+	}
+	
+}
