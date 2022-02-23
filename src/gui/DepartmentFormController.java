9800006d097ffe218f08f1d3b648a685baace011
unknown <fabriciorochapalmeira@gmail.com>
2022-02-23 17:41:09 -0300

    Seller list

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -23,7 +23,7 @@ import model.entities.Department;
 import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
-public class DeparmentFormController implements Initializable {
+public class DepartmentFormController implements Initializable {
 
 	private Department entity;
 
